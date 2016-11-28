b3a8b8ae3d107ffe20109452c590fbf1a6b7aa93
Adam Fidel <refuse@gmail.com>
2016-11-27 22:38:16 -0600

    Some word based analysis of all letters. Need to remove dates and numbers.

--- analysis/words.R
+++ analysis/words.R
@@ -1,1 +1,27 @@
 library(tidytext)
+library(ggplot2)
+library(jsonlite)
+
+data("stop_words")
+
+loadFiles <- function() {
+  files <- list.files("../scrape/data/json/")
+  text <- sapply(files, function(file) { fromJSON(paste("../scrape/data/json/",file,sep=""))$Transcript })
+  text_df <- data_frame(line = 1:83, text = text)
+  return(text_df)
+}
+
+
+text_df <- loadFiles()
+
+counts <- text_df %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words) %>%
+  count(word, sort = T)  %>%
+  filter(n > 40) %>%
+  mutate(word = reorder(word, n))
+
+ggplot(counts, aes(word, n)) +
+  geom_bar(stat = "identity") +
+  xlab(NULL) +
+  coord_flip()
